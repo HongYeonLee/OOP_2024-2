376a792524ad3fe4ff4bf8748896b665245719b2
HongYeonLee <llhongyan@naver.com>
2024-12-05 08:27:03 +0900

    파일 읽고 쓰기, 해시 맵

---
+++ src/week11/MainEx.java
@@ -0,0 +1,11 @@
+package week11;
+
+public class MainEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu ewha = new Menu();
+		ewha.run("이화여자대학교 컴퓨터공학과 2371049 이홍연");
+	}
+
+}
